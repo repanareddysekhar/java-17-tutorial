74192e62d497e2c587b2e94dc81f497ad8880297
repananiromoney <repana@niro.money>
2024-03-21 13:06:46 +0530

    Example of custom annotations

---
+++ src/main/java/spring/UnderTest.java
@@ -0,0 +1,12 @@
+package spring;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface UnderTest {
+    String value() default "";
+}
